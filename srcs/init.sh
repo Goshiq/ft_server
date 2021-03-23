62846fb4596a547a298bd68d6a92627054d44ac5
jmogo <jmogo@student.21-school.ru>
2021-03-23 19:37:16 +0300

    wtf is going on

--- srcs/init.sh
+++ srcs/init.sh
@@ -4,8 +4,8 @@ service php7.3-fpm start
 service nginx start
 service mysql start
 
-echo "CREATE DATABASE Artist;" | mysql -u root --skip-password
-echo "GRANT ALL PRIVILEGES ON Artist.* TO 'root'@'localhost' WITH GRANT OPTION;" | mysql -u root --skip-password
+echo "CREATE DATABASE Jmogo;" | mysql -u root --skip-password
+echo "GRANT ALL PRIVILEGES ON Jmogo.* TO 'root'@'localhost' WITH GRANT OPTION;" | mysql -u root --skip-password
 echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
 echo "UPDATE mysql.user SET PLUGIN='' WHERE user='root';" | mysql -u root --skip-password
 
--- srcs/wp-config.php
+++ srcs/wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'Artist' );
+define( 'DB_NAME', 'Jmogo' );
 
 /** MySQL database username */
 define( 'DB_USER', 'root' );
